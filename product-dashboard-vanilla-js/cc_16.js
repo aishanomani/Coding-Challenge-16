24e32e3aaab0ad4932e4b57218632cec253963db
Aisha Nomani <aishan@usf.edu>
2025-03-27 23:20:01 -0400

    Implement fetchProductsThen using .then() and .catch()

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -1,3 +1,23 @@
 // Task 1: Set Up the Project Structure
 console.log('****Initialize Basic HTML Structure****');
 
+// Task 2: Fetch Products with .then()
+function fetchProductsThen() {
+    fetch('https://www.course-api.com/javascript-store-products')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to Fetch Products: ${response.status} ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((products) => {
+        products.forEach((product) => {
+          console.log(product.fields.name);
+        });
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+      });
+  }
+  fetchProductsThen();
+  console.log('****Implement fetchProductsThen using .then() and .catch()****');
