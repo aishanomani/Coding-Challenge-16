fc8fa93ba84f122f1a0bf8d5f630bc2291ed4443
Aisha Nomani <aishan@usf.edu>
2025-03-27 23:22:00 -0400

    Add fetchProductsAsync using async/await and try/catch

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -21,3 +21,26 @@ function fetchProductsThen() {
   }
   fetchProductsThen();
   console.log('****Implement fetchProductsThen using .then() and .catch()****');
+
+  // Task 3: Fetch Products with async/await
+  async function fetchProductsAsync() {
+    try {
+      const response = await fetch('https://www.course-api.com/javascript-store-products');
+      if (!response.ok) {
+        throw new Error(`Failed to Fetch Products: ${response.status} ${response.statusText}`);
+      }
+      const products = await response.json();
+      displayProducts(products);
+    } catch (error) {
+      handleError(error);
+    }
+  }
+  function handleError(error) {
+    const container = document.getElementById('product-container');
+    container.innerHTML = `<p class="error">Error Loading Products: ${error.message}</p>`;
+    console.error('Fetch error:', error);
+  }
+  fetchProductsAsync();
+  console.log('****Add fetchProductsAsync using async/await and try/catch****');
+
+  
